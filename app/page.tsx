37ae351daec31158f60dbbb424cdc342082f103b
Ivan Schuetz <ivanschuetz@gmail.com>
2025-01-01 11:30:13 +0100

    add imgs

--- app/page.tsx
+++ app/page.tsx
@@ -31,11 +31,12 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src="/hero.svg"
           alt="Next.js logo"
           width={180}
           height={38}
           priority
+          className="w-full"
         />
         <Projects />
 
