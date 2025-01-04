7200c130bdcfaa5c2a82a68bd972565bd6ffdef3
Ivan Schuetz <ivanschuetz@gmail.com>
2025-01-04 19:02:06 +0100

    update descriptions

--- app/model.tsx
+++ app/model.tsx
@@ -1,44 +1,32 @@
-const lorem =
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-
 export const load_projects = (): ProjectType[] => {
   return [
     {
       id: "25",
       title: "Peer finder",
-      descr: "UWB/Bluetooth + cryptographic peer matcher for Apple devices",
-    },
-    {
-      id: "25",
-      title: "This homepage",
-      descr: "Next js etc",
+      descr:
+        "UWB/Bluetooth + cryptographic peer finder for Apple devices, did everything.",
     },
     {
       id: "24",
       title: "Density clouds simulation",
-      descr: "TODO copy from readme",
+      descr: "GPU accelerated simulation of density clouds",
     },
     {
       id: "23",
       title: "Hydrogen atom",
       descr:
-        "A visualization of the hydrogen atom orbitals, using slices rendered in a computer shader.",
-    },
-    {
-      id: "24",
-      title: "Rust android ios example app",
-      descr: lorem,
+        "Visualization of the hydrogen atom orbitals, using slices rendered in a computer shader.",
     },
     {
       id: "22",
       title: "Algorand SDK",
       descr:
-        "Was the main contributor for about a year. Ported all the features from the main (Go) SDK to Rust, making them idiomatic.",
+        "Main contributor for about a year. Ported all the features from the Go SDK to (idiomatic) Rust.",
     },
     {
       id: "21",
       title: "Capi",
-      descr: `Led and developed all the code for a startup to decentralize company shares on blockchain`,
+      descr: `Led a startup and wrote all the code to create and manage decentralized company shares on blockchain`,
     },
     {
       id: "21",
@@ -55,109 +43,106 @@ export const load_projects = (): ProjectType[] => {
       id: "19",
       title: "Co-Epi",
       descr:
-        "Developed open source iOS and Android apps for Covid contact tracing (custom, Apple/Google contact tracing frameworks were not existent yet). Implemented cryptographic protocol and participated in development of bluetooth libraries. Extracted the shared part of the apps into a Rust library. Optimized workflows for team not familiar with Rust. Directed extremely diverse team, including professional developers, scientists, students and diverse enthusiasts.",
+        "Developed iOS and Android apps for Covid bluetooth contact tracing. Implemented cryptographic protocol. Extracted shared core of apps to a Rust library.",
     },
     {
       id: "18",
       title: "Volvo cars",
       descr:
-        "Developed an Android app from scratch until 1.0 release. Partly directed team. Kotlin, RxJava, small team, code reviews via Bitbucket, continuous integration.  Developed an Android app from scratch until 1.0 release. Partly directed team. Kotlin, RxJava, small team, code reviews via Bitbucket, continuous integration.",
+        "Developed an Android/Kotlin app from scratch using the usual 'latest and greatest' frameworks and practices. Partly directed team.",
     },
     {
       id: "17",
       title: "Babbel",
       descr:
-        "Implemented new authentication and in-app-purchases flows for the iOS app. Large team, code reviews via Github, continuous integration, etc.",
+        "Implemented new authentication and in-app-purchases flows for the iOS app.",
     },
     {
       id: "16",
       title: "Wohin-mit",
       descr:
-        "Developed a mini search engine to figure out how to dispose properly of waste items or donate/sell. React, Javascript, Spring, Kotlin, Postgres. It got some attention from local politicians :D https://twitter.com/GYGeorg/status/1158323606201520129",
+        "Developed a mini search engine with Kotlin/Spring and React to dispose properly of waste items or donate/sell.",
     },
     {
       id: "15",
       title: "Whisper",
       descr:
-        "Consulting and prototyping of mobile apps for a hearing aid (bluetooth). Compared technologies to decide what to use (React Native, Flutter, native, etc.), designed and developed bluetooth framework and foundation for Android app. Delivered a fully functional prototype.",
+        "Developed fully functional prototype of a companion mobile app for a hearing aid (bluetooth). Initial consulting to decide tech stack.",
     },
     {
       id: "14",
       title: "WWF",
       descr:
-        "Developed features for a GIS (geographic information system) app used to conduct surveys in forests. QT framework, AppStudio for ArgGIS. Developed features for a GIS (geographic information system) app used to conduct surveys in forests. QT framework, AppStudio for ArgGIS ",
+        "Developed features for a GIS app used to conduct surveys in forests with QT framework, AppStudio for ArgGIS",
     },
     {
       id: "13",
       title: "Otto",
       descr:
-        "Developed features for Android apps primarily in a common core used by 7 or so client companies. Mid size team, code reviews on Github, architecture intensive to design optimal interfaces / modularity between core and companies. Mostly remote, occasional traveling to Hamburg for some meetings.",
+        "Developed features for Android apps with a common core used by multiple client companies.",
     },
     {
       id: "12",
       title: "Groma",
       descr:
-        'Learning project for iOS development turned into "for-profit". It was a grocery list app with many additional features, like budget and inventory management. I wrote from scratch a real time synchronization backend (Scala / Slick / Play framework) to share lists, and offline-first functionality (to use the app without internet and sync when connected).',
+        "Developed a fairly advanced grocery app with real time sync, offline-first and innovative UX. Directed graphic contractor.",
     },
     {
       id: "11",
       title: "Cashquizz",
-      descr:
-        "Feature development for a quiz / game iOS app. RxSwift, very custom UIs, rest apis, git.",
+      descr: "Developed feaatures for a quiz / game iOS app, using RxSwift.",
     },
     {
       id: "10",
       title: "7Mind",
       descr:
-        "Developed a meditation Android app from scratch to release (or near, don't remember), and participated in development of existing iOS app. Small team, code reviews, rest apis, audio, file management / caching, very custom UIs / animations, reactive frameworks.",
+        "Developed meditation Android from scratch, and iOS apps. Android app from scratch to release. Reactive frameworks, etc.",
     },
     {
       id: "9",
       title: "Optris",
       descr:
-        "Development and design of MVP to configure temperature sensors over USB and display temperatures in real time on charts. From scratch, Didn't have previous experience with USB programming, sensors or general environment, delivered well working MVP. Android, Java, USB, sensors, data visualization, MPAndroidChart, SciChart, Cradle, Fastlane, Google Play alpha/beta distribution.",
+        "Developed an MVP from scratch to configure temperature sensors over USB and display temperatures in real time on charts.",
     },
     {
       id: "8",
       title: "Grafiti LLC",
       descr:
-        "iOS app development and customizations of an open source charts library which I created. iOS, Swift, Git, JIRA, CocoaPods, Fastlane, CircleCI, HockeyApp, Crashlytics, Testflight",
+        "iOS app development and customizations of an open source charts library I created.",
     },
     {
       id: "7",
       title: "Snow measurement app",
       descr:
-        "Development of a data visualization app for snow depth / temperature measurements and necessary customizations of the used open source charts library, which I created.",
+        "Development from scratch of a data visualization app for snow depth / temperature measurements and customizations of open source charts library I created.",
     },
     {
       id: "6",
       title: "Quandoo",
-      descr:
-        "iOS app development for booking / rating of restaurants. Rest apis, git, etc.",
+      descr: "iOS app development for booking / rating of restaurants.",
     },
     {
       id: "5",
       title: "EGym",
-      descr:
-        "Development of a fitness app for iPhone and iPad. Rest api, git, etc.",
+      descr: "Development of a fitness app for iPhone and iPad.",
     },
     {
       id: "4",
       title: "Rocket Internet",
       descr:
-        "Development of diverse commerce Android apps. Rest apis, caching and sync for offline usage, performance optimization to make things load fast. Push notifications, Git, Jira, etc. Directed an international team (mostly Europe / Asia), code reviews.",
+        "Development of diverse commerce Android apps with demanding UIs (offline first, performance).",
     },
     {
       id: "3",
       title: "Cuponation",
       descr:
-        "Development of Android app. maps-intensive, came up with / developed some custom gestures, Rest api, Internationalization, custom UI elements / animations. Participated in product, design and market research a little.",
+        "Development of Android app. maps-intensive, came up with custom gestures, also did some product management.",
     },
     {
       id: "2",
       title: "Dropgifts",
       descr:
-        "Development of an Android app about sending gifts to friends via Facebook api. Did everything. Rest apis, very custom UIs, git, push notifications, localization. Parts of this app can be still seen e.g. here https://springwise.com/site-lets-facebook-friends-send-gift-cards/",
+        "Development from scratch of an Android app about sending gifts to friends via Facebook api.",
     },
     {
       id: "1",
